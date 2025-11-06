93d8b93a79757951098864b1b6d2e2a2cc51af37
w4sk <guiguidubian@gmail.com>
2025-11-06 15:02:10 +0900

    create next env

---
+++ src/app/page.js
@@ -0,0 +1,12 @@
+'use client';
+
+import dynamic from "next/dynamic";
+
+const AframeScene = dynamic(() => import("../components/AframeScene"), {
+  ssr: false,
+  loading: () => <div className="flex h-screen w-screen items-center justify-center">Loading scene...</div>,
+});
+
+export default function Home() {
+  return <AframeScene />;
+}
