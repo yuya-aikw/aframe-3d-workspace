6ec61af69c3c176c25df8d85d4c09548b240a0c4
yuyaa199908 <yuyaa199908@gmail.com>
2026-01-16 01:00:00 +0900

    fix ros2_websocket.sh path

--- ros2_websocket.sh
+++ ros2_websocket.sh
@@ -1,5 +1,4 @@
-# ros2 launch rosbridge_server rosbridge_websocket_launch.xml
 ros2 launch rosbridge_server rosbridge_websocket_launch.xml \
   ssl:=true \
-  certfile:=/home/aichi2204/jms2025_demo/certificates/localhost.pem \
-  keyfile:=/home/aichi2204/jms2025_demo/certificates/localhost-key.pem
+  certfile:=$PWD/certificates/localhost.pem \
+  keyfile:=$PWD/certificates/localhost-key.pem
